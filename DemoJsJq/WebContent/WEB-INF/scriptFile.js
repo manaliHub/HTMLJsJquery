4fee3b5fa59671bd7011ca8ecf20d008ebe3a5d9
prana <prana@10.0.0.45>
2020-12-24 16:33:08 +1100

    FirstCommit of HTml JS and Jquery

---
+++ DemoJsJq/WebContent/WEB-INF/scriptFile.js
@@ -0,0 +1,14 @@
+// Write Javascript code!
+
+function clicked(){
+	
+	var name=document.getElementById("nameText");
+	name.value="Aditya";
+	name.setAttribute("style","color:red");
+	
+}
+
+function sumFunction(num1,num2){
+	var sum = parseInt(num1)+parseInt(num2);
+	return sum;
+}
