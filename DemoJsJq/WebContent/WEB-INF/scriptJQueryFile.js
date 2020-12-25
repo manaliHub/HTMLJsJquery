1fb5af3cde8f0bd90fb01d7d4f1d582247a2a267
prana <prana@10.0.0.45>
2020-12-25 15:43:37 +1100

    added jquery file and js file

---
+++ DemoJsJq/WebContent/WEB-INF/scriptJQueryFile.js
@@ -0,0 +1,12 @@
+// Write Javascript code!
+
+$(document).ready(function(){
+		//$("#nameTxt").val("welcome all");
+		console.log("Document is ready !!");
+	
+	});
+
+function sumFunction(num1,num2){
+	var sum = parseInt(num1)+parseInt(num2);
+	return sum;
+}
