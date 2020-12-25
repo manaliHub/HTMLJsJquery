e4be32322701b2ebe297a60ac533d80f79e7edf0
prana <prana@10.0.0.45>
2020-12-25 19:48:12 +1100

    some event handling on elements

--- DemoJsJq/WebContent/WEB-INF/scriptJQueryFile.js
+++ DemoJsJq/WebContent/WEB-INF/scriptJQueryFile.js
@@ -1,10 +1,6 @@
 // Write Javascript code!
 
-$(document).ready(function(){
-		//$("#nameTxt").val("welcome all");
-		console.log("Document is ready !!");
-	
-	});
+
 
 function sumFunction(num1,num2){
 	var sum = parseInt(num1)+parseInt(num2);
